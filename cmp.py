8f5c1f0d91857df87f31cf37fd7834c085cfa425
jotawarsd <64022045+jotawarsd@users.noreply.github.com>
2021-08-25 16:33:18 +0530

    comparison

---
+++ cmp.py
@@ -0,0 +1,11 @@
+a = int(input("a = "))
+b = int(input("b = "))
+c = int(input("c = "))
+
+if (a > b and a > c):
+    print("a is greatest")
+elif (b > c):
+    print("b is greatest")
+else:
+    print("c is greatest")
+    
