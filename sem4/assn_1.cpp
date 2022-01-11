13c6401a30d12b6873db4c8300dfc4de365c6630
jotawarsd <64022045+jotawarsd@users.noreply.github.com>
2022-01-11 17:28:47 +0530

    sem_4

---
+++ sem4/assn_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+	int flag;
+	char name[20];
+	node *down;
+	node *next;
+};
+
+class GLL
+{
+	node *head = NULL, *temp = NULL;
+	node *ptr = NULL, t1 = NULL;
+	
+
+};
